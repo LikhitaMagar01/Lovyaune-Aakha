23981cbdbd94b0e8b5e4dbefd0b4b5bbe39d46c8
LikhitaMagar01 <leekhitame@gmail.com>
2024-09-30 22:54:04 +0545

    style: create page designs for works, products

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -2,6 +2,7 @@
 
 import { ParallaxScroll } from '../components/ui/parallax-scroll';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link'; // Ensure to import Link
 
 const Design = () => {
   const images = [
@@ -23,7 +24,7 @@ const Design = () => {
   ];
 
   return (
-    <div className='flex flex-col bg-black text-white text-center'>
+    <div className='relative flex flex-col bg-black text-white text-center'>
       <h1 className='mt-8'>Wall Painting</h1>
       <div className={styles.scrollWrapper}>
         <ParallaxScroll className={styles.element} images={images} />
@@ -31,6 +32,26 @@ const Design = () => {
       <h1 className='mt-5'>House Painting</h1>
       <div className={`mb-5 ${styles.scrollWrapper}`}>
         <ParallaxScroll className={styles.element} images={house_images} />
+      </div>
+
+      {/* Links in the bottom right corner */}
+      <div className="absolute bottom-[148px] right-0 mr-4">
+        <div className="inline-block bg-white p-4 shadow-lg rounded-lg">
+          <ul className="flex flex-col space-y-2 text-black">
+            <li className="navItem">
+              <Link href="/works">Our Works</Link>
+            </li>
+            <li className="navItem">
+              <Link href="/designs">Designs</Link>
+            </li>
+            <li className="navItem">
+              <Link href="/products">Products</Link>
+            </li>
+            <li className="navItem">
+              <Link href="/about">About Us</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
